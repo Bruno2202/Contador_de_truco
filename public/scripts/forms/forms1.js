6a97c6d282ca83bd5487070bd7edac8622c4c95e
Bruno2202 <brunoterribile2005@gmail.com>
2023-06-26 23:49:44 -0300

    3.3.3

--- public/scripts/forms/forms1.js
+++ public/scripts/forms/forms1.js
@@ -1,6 +1,6 @@
 import { initializeApp } from "https://www.gstatic.com/firebasejs/9.22.2/firebase-app.js";
 import { getFirestore , collection , getDocs } from "https://www.gstatic.com/firebasejs/9.22.2/firebase-firestore.js";
-import { doc , updateDoc } from "https://www.gstatic.com/firebasejs/9.22.2/firebase-firestore.js";  
+import { doc , setDoc } from "https://www.gstatic.com/firebasejs/9.22.2/firebase-firestore.js";  
 
 const firebaseConfig = {
   apiKey: "AIzaSyCdAZCXyB49EuUJqTEwmoRsjmj-piKlyOg",
@@ -15,8 +15,7 @@ const firebaseConfig = {
 const app = initializeApp(firebaseConfig);
 const db = getFirestore(app);
 
-
-
+//Mostra a versão da aplicação
 getDocs(collection(db, "updates" ))
     .then(querySnapshot => {
         const version1 = querySnapshot.docs.reduce((acc, doc) => {
@@ -27,3 +26,29 @@ getDocs(collection(db, "updates" ))
         const version2 = document.querySelector("[data-js = 'version']")
         version2.innerHTML = version1
 })
+
+//Salva as novas informações no Firebase
+const configCollection = collection(db, "updates");
+
+export function salvarConfiguracoes(dados) {
+    const docRef = doc(configCollection, "n7tfA97uc3MQl6LgiBJ5");
+    return setDoc(docRef, dados);
+}
+
+document.getElementById('save-Changes').addEventListener('click', () => {
+    const text_Log = textLog;
+    const  new_Version = document.getElementById('new-Version').value;
+
+    const dados = {
+        Log: text_Log,
+        Version: new_Version
+    };
+
+    salvarConfiguracoes(dados)
+        .then(() => {
+            console.log('Configurações salvas com sucesso!');
+        })
+        .catch((error) => {
+            console.error('Erro ao salvar as configurações:', error);
+        });
+});
